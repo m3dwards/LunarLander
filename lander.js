87842d7200f0014e448e2e8af74c1b9f141efb83
maxwedwards <youwontforgetthis@gmail.com>
2016-11-19 17:04:55 +0000

    Update lander.js
    Debug

--- lander.js
+++ lander.js
@@ -24,7 +24,7 @@ $(function(){
 
 $(document).keydown(onKeyDown);
 $(document).keyup(onKeyUp);
-$(document).bind('touchstart', function(event){upDown = true;});
+$(document).bind('touchstart', function(event){alert('hello'); upDown = true;});
 $(document).bind('touchend', function(event){upDown = false;});
 
 function start() {
